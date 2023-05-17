dea97239485274ab1d8f3dd678dddcc0a0089ead
rakesh <rakraj1602@gmail.com>
2023-05-17 21:16:27 +0530

    last

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const mysql = require("mysql")
+const mysql = require("mysql2")
 const bodyParser = require("body-parser")
 const session = require("express-session")
 const encoder = bodyParser.urlencoded();
@@ -23,7 +23,10 @@ const connection = mysql.createPool({
     host:process.env.HOST,
     user:process.env.USER,
     database:process.env.DATABASE,
-    password:process.env.PASSWORD
+    password:process.env.PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 connection.getConnection((e) => {
